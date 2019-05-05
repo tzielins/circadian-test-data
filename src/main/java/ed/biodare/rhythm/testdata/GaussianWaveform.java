0042bb295543b1f33647d8a69f6d52f7e7076892
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2019-05-05 21:50:32 +0100

    gausian generator

--- src/main/java/ed/biodare/rhythm/testdata/GaussianWaveform.java
+++ src/main/java/ed/biodare/rhythm/testdata/GaussianWaveform.java
@@ -5,13 +5,95 @@
  */
 package ed.biodare.rhythm.testdata;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.function.Gaussian;
+import static org.apache.commons.math3.util.FastMath.*;
+
 /**
  *
  * @author tzielins
  */
-public class GaussianWaveform {
+public class GaussianWaveform implements UnivariateFunction {
     
-    public static GaussianWaveform create() {
-        return new GaussianWaveform();
+    public static GaussianWaveform create(double sigma, double phase) {
+        return new GaussianWaveform(sigma, phase);
     }
+    
+    public static GaussianWaveform create(double sigma) {
+        return create(sigma, 0);
+    }
+    
+    
+    final double sigma;
+    final double phase;
+    
+    final double max;
+    final double min;
+    final UnivariateFunction function;
+    
+    private double area;
+    
+    public GaussianWaveform(double sigma, double phase) {
+        
+        if (phase >= 1) {
+            throw new IllegalArgumentException("Gaussian waveform operates only "
+                    + "in [0,1) so the phase also has to be within this range");
+        }
+        
+        this.sigma = sigma;
+        this.phase = phase;
+        function = new Gaussian(0, sigma);
+        
+        double tmax = function.value(0);
+        min = min(function.value(0.5), function.value(-0.5));
+        max = tmax - min;
+               
+    }
+
+    @Override
+    public double value(double x) {
+        if (x < 0 || x > 1)
+            throw new IllegalArgumentException("Gaussian waveform operrates only in [0,1)");
+        x = x - phase;
+        if (x >= 0.5) {
+            x = x -1;
+        }
+        return (function.value(x)-min)/max;
+    }
+    
+    public double[] values(double[] x) {
+        double[] values = new double[x.length];
+        for (int i =0;i<x.length;i++) {
+            values[i] = value(x[i]);
+        }
+        return values;
+    }
+    
+    public List<Double> valuesList(List<Double> xs) {
+        List<Double> values = new ArrayList<>(xs.size());
+        for (Double x : xs) {
+            values.add(value(x));
+        }
+        return values;
+    }
+    
+    public double area() {
+        
+        if (area > 0) {
+            return area;
+        }
+        int N = 500;
+        double step = 0.5 / N;
+        double sum = -N * min;
+        for (int i = 0; i< N; i++) {
+            sum += function.value(i*step);
+        }
+        
+        area = (sum / max) / N;
+        return area;
+    }
+    
+    
 }
--- src/test/java/ed/biodare/rhythm/testdata/GaussianWaveformTest.java
+++ src/test/java/ed/biodare/rhythm/testdata/GaussianWaveformTest.java
@@ -5,6 +5,8 @@
  */
 package ed.biodare.rhythm.testdata;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -14,6 +16,8 @@ import static org.junit.Assert.*;
  * @author tzielins
  */
 public class GaussianWaveformTest {
+    
+    final double EPS = 1E-6;
     
     public GaussianWaveformTest() {
     }
@@ -25,8 +29,58 @@ public class GaussianWaveformTest {
     @Test
     public void testCreates() {
         
-        GaussianWaveform wave = GaussianWaveform.create();
+        GaussianWaveform wave = GaussianWaveform.create(0.1);
         assertNotNull(wave);
+        
+    }
+    
+    @Test
+    public void waveformHasMaxValue1AtPhaseAnd0AtTrough() {
+        
+        GaussianWaveform wave = GaussianWaveform.create(0.1, 0);
+        
+        assertEquals(1, wave.value(0), EPS);
+        assertEquals(1, wave.value(1), EPS);
+        assertEquals(0, wave.value(0.5), EPS);
+        
+        wave = GaussianWaveform.create(0.05, 0.5);
+        assertEquals(1, wave.value(0.5), EPS);
+        assertEquals(0, wave.value(1), EPS);
+        assertEquals(0, wave.value(0), EPS);
+        
+        wave = GaussianWaveform.create(0.5, 0.2);
+        assertEquals(1, wave.value(0.2), EPS);
+        assertEquals(0, wave.value(0.7), EPS);
+        
+        try {
+            double v = wave.value(1.1);
+            fail("Exception expected got: "+v);
+        } catch (IllegalArgumentException e) {}
+    }
+    
+    @Test
+    public void areaWorks() {
+        GaussianWaveform wave = GaussianWaveform.create(0.1, 0);
+        
+        double ar1 = wave.area();
+        assertTrue(ar1 > 0 && ar1 < 1);
+        
+        assertEquals(ar1, GaussianWaveform.create(0.1, 0.1).area(), EPS);
+        assertEquals(ar1, GaussianWaveform.create(0.1, 0.33).area(), EPS);
+        assertTrue(ar1 > GaussianWaveform.create(0.1-EPS, 0).area());
+        assertTrue(ar1 < GaussianWaveform.create(0.1+EPS, 0).area());
+    }
+    
+    @Test
+    public void values() {
+        GaussianWaveform wave = GaussianWaveform.create(0.2, 0);
+        
+        List<Double> values = Arrays.asList(0.0,0.5,1.0);
+        
+        assertEquals(Arrays.asList(1.0,0.0,1.0), wave.valuesList(values));
+        
+        double[] arr = values.stream().mapToDouble( v -> v).toArray();
+        assertArrayEquals(new double[]{1.0,0.0,1.0}, wave.values(arr), EPS);
         
     }
     
